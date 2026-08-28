59e871a944889c5f1a59d3987b3cea3394b639eb
Walex <walex@Samuels-MacBook-Pro.local>
2026-08-28 02:24:36 +0100

    feat(laravel): add public/index.php web entrypoint and .htaccess configuration

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+    require __DIR__.'/../vendor/autoload.php';
+}
+
+// Bootstrap Laravel and handle the request...
+if (file_exists(__DIR__.'/../bootstrap/app.php')) {
+    (require_once __DIR__.'/../bootstrap/app.php')
+        ->handleRequest(Request::capture());
+}
