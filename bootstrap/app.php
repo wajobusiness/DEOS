872652f8191228f597ff2bb2bda66398adf17d4a
Walex <walex@Samuels-MacBook-Pro.local>
2026-08-29 10:12:35 +0100

    fix(bootstrap): explicitly set application namespace App\ on Application instance

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,7 +25,7 @@ namespace {
     use Illuminate\Foundation\Configuration\Exceptions;
     use Illuminate\Foundation\Configuration\Middleware;
 
-    return Application::configure(basePath: dirname(__DIR__))
+    $app = Application::configure(basePath: dirname(__DIR__))
         ->withRouting(
             web: __DIR__.'/../routes/web.php',
             api: __DIR__.'/../routes/api.php',
@@ -43,4 +43,8 @@ namespace {
         ->withExceptions(function (Exceptions $exceptions) {
             //
         })->create();
+
+    $app->setNamespace('App\\');
+
+    return $app;
 }
