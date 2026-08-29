d0a8dc36e0a698a65d18765aa3cd217228e8ba6c
Walex <walex@Samuels-MacBook-Pro.local>
2026-08-29 09:55:12 +0100

    fix(entrypoint): add dynamic appRoot auto-detection in public/index.php for cPanel public_html deployment

--- public/index.php
+++ public/index.php
@@ -21,19 +21,27 @@ namespace {
 
     define('LARAVEL_START', microtime(true));
 
+    $appRoot = file_exists(__DIR__.'/vendor/autoload.php')
+        ? __DIR__
+        : (file_exists(__DIR__.'/../vendor/autoload.php')
+            ? __DIR__.'/..'
+            : (file_exists(__DIR__.'/../deos/vendor/autoload.php')
+                ? __DIR__.'/../deos'
+                : '/home/eviona/deos'));
+
     // Determine if the application is in maintenance mode...
-    if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    if (file_exists($maintenance = $appRoot.'/storage/framework/maintenance.php')) {
         require $maintenance;
     }
 
     // Register the Composer autoloader...
-    if (file_exists(__DIR__.'/../vendor/autoload.php')) {
-        require __DIR__.'/../vendor/autoload.php';
+    if (file_exists($appRoot.'/vendor/autoload.php')) {
+        require $appRoot.'/vendor/autoload.php';
     }
 
     // Bootstrap Laravel and handle the request...
-    if (file_exists(__DIR__.'/../bootstrap/app.php')) {
-        (require_once __DIR__.'/../bootstrap/app.php')
+    if (file_exists($appRoot.'/bootstrap/app.php')) {
+        (require_once $appRoot.'/bootstrap/app.php')
             ->handleRequest(Request::capture());
     }
 }
